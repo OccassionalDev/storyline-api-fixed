6d0752bbaa8d1692a9ebf400a090fc4e735dff11
OccassionalDev <ericaschimmel01@gmail.com>
2020-07-16 20:42:31 -0400

    added in old functionality of UsersController and added in current_user = user in create action to set a session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,34 @@
 class UsersController < ApplicationController
+    
+    def index
+        users = User.all 
+        render json: { users: users } 
+    end 
+
+    def create 
+        user = User.new(user_params)
+
+        if user.save 
+            current_user = user
+            render json: { user: user }
+        else 
+            render json: { errors: user.errors.full_messages }
+        end 
+    end 
+
+    def show
+        user = User.find(params[:id])
+
+        if user 
+            render json: { user: user } 
+        else 
+            render json: { errors: ['Could not find a page for that user.'] }
+        end
+    end 
+
+    private 
+
+    def user_params(*args)
+        params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end 
 end
