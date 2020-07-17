f96dcab6cb35de1a20340e3d477f2a06857b5d97
OccassionalDev <ericaschimmel01@gmail.com>
2020-07-16 20:47:54 -0400

    added in old functionality in CharactersController from old api and added in current_user instead of locating the user in create action to easily create a new character

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,59 @@
 class CharactersController < ApplicationController
+    before_action :find_character, only: [:show, :update, :destroy]
+
+    def index
+        if params[:user_id]
+            characters = Character.where(user_id: params[:user_id])
+        else 
+            characters = Character.all 
+        end 
+
+        render json: characters, include: ["story", "user"]
+    end 
+
+    def recently_created_characters
+        recent_characters = Character.recently_created_characters
+
+        render json: recent_characters, include: ["story", "user"]
+    end
+
+    def create
+        character = current_user.characters.create(character_params)
+        
+        if character.save
+            render json: { user: current_user, character: character }
+        else 
+            render json: { errors: character.errors.full_messages }
+        end 
+    end 
+
+    def show
+        if @character
+            render json: { character: @character, user: @character.user, story: @character.story, comments: @character.comments } 
+        else 
+            render json: { errors: ['Character could not be found'] }
+        end 
+    end 
+
+    def update
+        if @character.update(character_params)
+            render json: @character 
+        else 
+            render json: { errors: @character.errors.full_messages }
+        end 
+    end 
+
+    def destory
+        @character.destory 
+    end 
+
+    private
+
+    def find_character
+        @character = Character.find_by_id(params[:id])
+    end 
+
+    def character_params(*args)
+        params.require(:character).permit(:name, :age, :gender, :species, :overview, :personality, :story_id)
+    end 
 end
