efa6e0d26d57aab977f120ba3916e7c3a3a5b3e9
OccassionalDev <ericaschimmel01@gmail.com>
2020-07-16 20:27:28 -0400

    added in helper methods of current_user, logged_in? and require_login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :logged_in, :require_login
+
+    def current_user=(user)
+        session[:user_id] = user.id 
+    end 
+
+    def current_user 
+        User.find_by_id(session[:user_id])
+    end 
+
+    def logged_in?
+        current_user != nil
+    end 
+
+    private
+
+    def require_login
+        render json: { errors: ["You must be logged in to do that action."] } unless logged_in?
+    end 
 end
