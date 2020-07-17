807d19c448c01deb6955325badbddf0a4c1896dc
OccassionalDev <ericaschimmel01@gmail.com>
2020-07-16 20:39:46 -0400

    added in old functionality of SessionsController from old api and added in current_user = user to set a session upon login and destroy action to clear the session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,16 @@
-class SessionsController < ApplicationController
+class SessionsController < ApplicationController 
+    def create
+        user = User.find_by_email(params[:email])
 
+        if user && user.authenticate(params[:password])
+            current_user = user
+            render json: { user: user }
+        else 
+            render json: { errors: ['Invalid email or password.'] }
+        end 
+    end  
+
+    def destory
+        session.delete :user_id
+    end 
 end 
